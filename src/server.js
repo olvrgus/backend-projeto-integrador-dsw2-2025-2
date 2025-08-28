8a5d9d131dd080186c3652f90bd93b4b19a3782f
olvrgus <go0694447@gmail.com>
2025-08-28 15:48:11 -0300

    aula passada

--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ const app = express();
 app.get("/", (req, res) => {
     res.send("olá mundo");
 });
+app.get("/html", (req,res) => {
+    res.send("<h1>teste</h1><p>Parágrafo</p>");
+});
 app.listen(3000, () => 
     console.log(`Servidor rodando em http://localhost:3000`)
 );
