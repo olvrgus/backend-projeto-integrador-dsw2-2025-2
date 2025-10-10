def6d7b0a20e8b0648413e539197403e18ed7661
olvrgus <go0694447@gmail.com>
2025-10-10 17:11:03 -0300

    aaaaaaaa

--- src/db.js
+++ src/db.js
@@ -6,11 +6,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new Pool({
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  database: process.env.PGDATABASE,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
 });
 
 export { pool };
