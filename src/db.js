e1f53ed80c49d9f61255b0f8a2e7175a06b8ba28
olvrgus <go0694447@gmail.com>
2025-10-17 16:53:42 -0300

    só a massa

--- src/db.js
+++ src/db.js
@@ -6,11 +6,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  host: process.env. DB_HOST,
+  port: process.env. DB_PORT,
+  database: process.env. DB_DATABASE,
+  user: process.env. DB_USER,
+  password: process.env. DB_PASSWORD,
 });
 
 export { pool };
